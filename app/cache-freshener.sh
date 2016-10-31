ebd083bc7d10ca3354d08c6c2644fb54be316fb1
Dave North <dnorth@signiant.com>
2016-10-31 13:33:20 -0400

    remove the json suffix

--- app/cache-freshener.sh
+++ app/cache-freshener.sh
@@ -10,7 +10,7 @@ echo "Connecting to the IP list service on port ${PORT}"
 ENDPOINTS=$(curl -s ${LINKED_CONTAINER_NAME}:${PORT} | grep li |awk -F '[<>]' '{print $5}')
 
 # write the base html file
-curl -s ${LINKED_CONTAINER_NAME}:${PORT} | sed -e 's/">/\.json">/g' > /json/index.html
+curl -s ${LINKED_CONTAINER_NAME}:${PORT} > /json/index.html
 
 # Loop forever, sleeping for our frequency
 while true
@@ -22,7 +22,7 @@ do
     echo "processing endpoint ${ENDPOINT}"
     # curl -s -w "\nTimings: connect:%{time_connect} starttransfer:%{time_starttransfer} total:%{time_total}\n" ${LINKED_CONTAINER_NAME}:${PORT}/${ENDPOINT}
     curl -s ${LINKED_CONTAINER_NAME}:${PORT}/${ENDPOINT} > /json/${ENDPOINT}.tmp
-    mv /json/${ENDPOINT}.tmp /json/${ENDPOINT}.json
+    mv /json/${ENDPOINT}.tmp /json/${ENDPOINT}
   done
 
   sleep 30
